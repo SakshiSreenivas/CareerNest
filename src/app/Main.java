2dcb7ea61df5eb77b112d50ca5f3bb207fafb445
SakshiSreenivas <sakshisreenivas214@gmail.com>
2026-01-26 09:53:25 +0530

    Day 4: Add menu-driven flow to Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,19 +1,48 @@
 package app;
 
+import java.util.Scanner;
 import model.Student;
 import model.Recruiter;
 import service.UserService;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         UserService service = new UserService();
 
-        Student s1 = new Student(1, "Alice", "alice@mail.com", "CSE", 3);
-        Recruiter r1 = new Recruiter(101, "Bob", "bob@company.com", "TechCorp");
+        while (true) {
+            System.out.println("\n--- CareerNest Menu ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. Add Recruiter");
+            System.out.println("3. View All Users");
+            System.out.println("4. Exit");
+            System.out.print("Enter choice: ");
 
-        service.addUser(s1);
-        service.addUser(r1);
+            int choice = sc.nextInt();
 
-        service.displayAllUsers();
+            switch (choice) {
+                case 1:
+                    Student s = new Student(1, "Alice", "alice@mail.com", "CSE", 3);
+                    service.addUser(s);
+                    break;
+
+                case 2:
+                    Recruiter r = new Recruiter(101, "Bob", "bob@company.com", "TechCorp");
+                    service.addUser(r);
+                    break;
+
+                case 3:
+                    service.displayAllUsers();
+                    break;
+
+                case 4:
+                    System.out.println("Exiting CareerNest...");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
     }
 }
