427d04094a48bd15dd0d664506062df9e35ba53f
SakshiSreenivas <sakshisreenivas214@gmail.com>
2026-02-15 08:52:03 +0530

    Introduce role attribute in User model

--- src/model/User.java
+++ src/model/User.java
@@ -6,11 +6,14 @@ public class User implements Serializable {
     protected int id;
     protected String name;
     protected String email;
+    protected String role;
+
 
     public User(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email; 
+        this.role = role; 
     }
 
     public int getId() {
@@ -20,6 +23,9 @@ public class User implements Serializable {
     // ✅ NEW SETTER
     public void setEmail(String email) {
         this.email = email;
+    }
+    public String getRole() {
+    return role;
     }
 
     public void displayInfo() {
