80253fc13893e1b9b438f5aad1fb037de1363c5b
SakshiSreenivas <sakshisreenivas214@gmail.com>
2026-02-16 19:01:54 +0530

    Upgrade password encryption to SHA-256 hashing

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -18,8 +18,31 @@ public UserService() {
 
 //  PASSWORD ENCRYPTION
 private String encryptPassword(String password) {
-    return Integer.toHexString(password.hashCode());
+
+    try {
+
+        MessageDigest md =
+            MessageDigest.getInstance("SHA-256");
+
+        byte[] hash =
+            md.digest(password.getBytes());
+
+        StringBuilder hex =
+            new StringBuilder();
+
+        for (byte b : hash) {
+            hex.append(
+                String.format("%02x", b)
+            );
+        }
+
+        return hex.toString();
+
+    } catch (Exception e) {
+        throw new RuntimeException(e);
+    }
 }
+
 
 //  ADD USER
 public void addUser(User user) {
