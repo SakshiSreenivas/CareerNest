18e680d107fb215fa0df29b93a26c6cd571d65a0
SakshiSreenivas <sakshisreenivas214@gmail.com>
2026-02-23 16:16:35 +0530

    feat: add HomeController with root health check endpoint

---
+++ src/test/java/com/CareerNest/CareerNest/CareerNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CareerNest.CareerNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
